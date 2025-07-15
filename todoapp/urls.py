4a35a7a31297d086fce59057801946c55ad7a19b
Rayo0131 <motunrayo188@gmail.com>
2025-07-15 18:58:08 +0100

    Third commit

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
     path('about/', views.About, name="about"),
     path('services/', views.Services, name="services"),
     path('contact/', views.Contact, name="contact"),
+    path('signup/', views.Signup, name="signup"),
+    path('todo-list/', views.task_list, name="task_list"), 
+    path('task/new', views.task_create, name="task_create"),
+    path('task/delete/<int:task_id>/', views.task_delete, name="task_delete"),
+    
 ]
